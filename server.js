7f708096635145b8ed675268aeed8489f7dad5e3
xtiagord <tiagodalmolin028@gmail.com>
2024-09-09 11:06:29 -0300

    porta

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const ExcelJS = require('exceljs');
 
 // Inicializar o Express
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3002;
 
 // Criação da conexão
 const db = mysql.createConnection({
